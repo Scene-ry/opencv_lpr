83d3df6331cd6028587c4c0c1f6a939dffe78b19
James Wu <james.wu@cargosmart.com>
2016-09-13 17:34:27 +0800

    use findContours to get the crop area

--- opencv_lpr.core/process/preprocess/GetLicenseFromImage.cpp
+++ opencv_lpr.core/process/preprocess/GetLicenseFromImage.cpp
@@ -32,87 +32,116 @@ void GetLicense(const cv::Mat& src, cv::Mat& dst, bool is_output_img, const char
         cv::imwrite(s_filename.c_str(), filter_hsv);
     }
 
-    // get edge
-    cv::Mat filter_edge;
-    cv::Canny(filter_hsv, filter_edge, 10, 90, 3);
-
+    cv::dilate(filter_hsv, filter_hsv, cv::Mat(cv::Size(5, 5), CV_8U));
+    //cv::erode(filter_hsv, filter_hsv, cv::Mat(cv::Size(5, 5), CV_8U));
     if (is_output_img)
     {
-        std::string s_filename = std::string(output_img_path) + "_canny.jpg";
-        cv::imwrite(s_filename.c_str(), filter_edge);
+        std::string s_filename = std::string(output_img_path) + "_hsvfilter_dilate.jpg";
+        cv::imwrite(s_filename.c_str(), filter_hsv);
     }
 
-    // construct histogram
-    int *vertical_hist = new int[filter_edge.rows];
-    int *horizontal_hist = new int[filter_edge.cols];
-    memset(vertical_hist, 0, sizeof(int) * filter_edge.rows);
-    memset(horizontal_hist, 0, sizeof(int) * filter_edge.cols);
-    for (cv::MatIterator_<uchar> it = filter_edge.begin<uchar>(); it != filter_edge.end<uchar>(); ++it)
+    std::vector<cv::Mat> contours;
+    cv::findContours(filter_hsv, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+    for (std::vector<cv::Mat>::iterator it = contours.begin(); it != contours.end(); it++)
     {
-        int pixel = *it;
-        if (pixel >= WHITE_THRESHOLD)
+        cv::Rect r = boundingRect(*it);
+
+        double width_height_rate = r.width / (double)r.height;
+        if (width_height_rate > 2.5 && width_height_rate < 4.5 && r.height > 20)
         {
-            vertical_hist[it.pos().y]++;
-            horizontal_hist[it.pos().x]++;
+            cv::Range rg_row, rg_col;
+            rg_row.start = r.y;
+            rg_row.end = r.y + r.height;
+            rg_col.start = r.x;
+            rg_col.end = r.x + r.width;
+            dst = src(rg_row, rg_col);
+            return;
         }
     }
 
-    // get average
-    int h_max = 0, h_min = INT_MAX, w_max = 0, w_min = INT_MAX;
-    for (int i = 0; i < filter_edge.rows; i++)
-    {
-        if (vertical_hist[i] > h_max)
-            h_max = vertical_hist[i];
-        if (vertical_hist[i] < h_min)
-            h_min = vertical_hist[i];
-    }
-    for (int i = 0; i < filter_edge.cols; i++)
-    {
-        if (horizontal_hist[i] > w_max)
-            w_max = horizontal_hist[i];
-        if (horizontal_hist[i] < w_min)
-            w_min = horizontal_hist[i];
-    }
+    dst = src.clone();
 
-    int h_threshold = (h_min + h_max) / 2, w_threshold = (w_min + w_max) / 2;
-
-    // locate the plate
-    int h_start = -1, h_end = -1, w_start = -1, w_end = -1;
-    for (int h = 0; h < filter_edge.rows; h++)
-    {
-        if (vertical_hist[h] >= h_threshold)
-        {
-            if (h_start == -1)
-            {
-                h_start = h;
-                continue;
-            }
-            if (h_start != -1)
-                h_end = h;
-        }
-    }
-    for (int w = 0; w < filter_edge.cols; w++)
-    {
-        if (horizontal_hist[w] >= w_threshold)
-        {
-            if (w_start == -1)
-            {
-                w_start = w;
-                continue;
-            }
-            if (w_start != -1)
-                w_end = w;
-        }
-    }
-
-    cv::Range rg_row, rg_col;
-    rg_row.start = h_start;
-    rg_row.end = h_end;
-    rg_col.start = w_start;
-    rg_col.end = w_end;
-
-    dst = cv::Mat(src, rg_row, rg_col);
-
-    delete[] vertical_hist;
-    delete[] horizontal_hist;
+    //// get edge
+    //cv::Mat filter_edge;
+    //cv::Canny(filter_hsv, filter_edge, 10, 90, 3);
+    //
+    //if (is_output_img)
+    //{
+    //    std::string s_filename = std::string(output_img_path) + "_canny.jpg";
+    //    cv::imwrite(s_filename.c_str(), filter_edge);
+    //}
+    //
+    //// construct histogram
+    //int *vertical_hist = new int[filter_edge.rows];
+    //int *horizontal_hist = new int[filter_edge.cols];
+    //memset(vertical_hist, 0, sizeof(int) * filter_edge.rows);
+    //memset(horizontal_hist, 0, sizeof(int) * filter_edge.cols);
+    //for (cv::MatIterator_<uchar> it = filter_edge.begin<uchar>(); it != filter_edge.end<uchar>(); ++it)
+    //{
+    //    int pixel = *it;
+    //    if (pixel >= WHITE_THRESHOLD)
+    //    {
+    //        vertical_hist[it.pos().y]++;
+    //        horizontal_hist[it.pos().x]++;
+    //    }
+    //}
+    //
+    //// get average
+    //int h_max = 0, h_min = INT_MAX, w_max = 0, w_min = INT_MAX;
+    //for (int i = 0; i < filter_edge.rows; i++)
+    //{
+    //    if (vertical_hist[i] > h_max)
+    //        h_max = vertical_hist[i];
+    //    if (vertical_hist[i] < h_min)
+    //        h_min = vertical_hist[i];
+    //}
+    //for (int i = 0; i < filter_edge.cols; i++)
+    //{
+    //    if (horizontal_hist[i] > w_max)
+    //        w_max = horizontal_hist[i];
+    //    if (horizontal_hist[i] < w_min)
+    //        w_min = horizontal_hist[i];
+    //}
+    //
+    //int h_threshold = (h_min + h_max) / 2, w_threshold = (w_min + w_max) / 2;
+    //
+    //// locate the plate
+    //int h_start = -1, h_end = -1, w_start = -1, w_end = -1;
+    //for (int h = 0; h < filter_edge.rows; h++)
+    //{
+    //    if (vertical_hist[h] >= h_threshold)
+    //    {
+    //        if (h_start == -1)
+    //        {
+    //            h_start = h;
+    //            continue;
+    //        }
+    //        if (h_start != -1)
+    //            h_end = h;
+    //    }
+    //}
+    //for (int w = 0; w < filter_edge.cols; w++)
+    //{
+    //    if (horizontal_hist[w] >= w_threshold)
+    //    {
+    //        if (w_start == -1)
+    //        {
+    //            w_start = w;
+    //            continue;
+    //        }
+    //        if (w_start != -1)
+    //            w_end = w;
+    //    }
+    //}
+    //
+    //cv::Range rg_row, rg_col;
+    //rg_row.start = h_start;
+    //rg_row.end = h_end;
+    //rg_col.start = w_start;
+    //rg_col.end = w_end;
+    //
+    //dst = cv::Mat(src, rg_row, rg_col);
+    //
+    //delete[] vertical_hist;
+    //delete[] horizontal_hist;
 }
